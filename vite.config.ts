262da46c235fcc348910bb94b14b18149784a4ac
Job <unwanaiso@gmail.com>
2026-01-22 16:15:04 +0100

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(),tailwindcss(), ],
+  
   define: {
-    // This tells the browser that __BASE_PATH__ is just an empty string for now
-    '__BASE_PATH__': JSON.stringify('') 
+    base: '/prompt-engineering-portfolio/',
   }
 })
+
